e9bb8895317f352de11a495a50d9f15fba732a6c
marcio-osshiro <marcio.osshiro@ifms.edu.br>
2019-09-27 19:12:08 -0400

    27 de setembro

--- web3Laravel/web3.sql
+++ web3Laravel/web3.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 13-Set-2019 às 17:20
+-- Tempo de geração: 27-Set-2019 às 20:10
 -- Versão do servidor: 10.3.17-MariaDB-0+deb10u1
--- versão do PHP: 7.3.4-2
+-- versão do PHP: 7.3.9-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -67,7 +67,40 @@ INSERT INTO `area` (`id`, `descricao`) VALUES
 (6, 'Programação Java'),
 (7, 'Matematica'),
 (16, 'Algoritmo'),
-(17, 'e-commerce');
+(17, 'e-commerce'),
+(19, 'nova area');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -78,27 +111,48 @@ INSERT INTO `area` (`id`, `descricao`) VALUES
 CREATE TABLE `professor` (
   `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `area_id` int(11) NOT NULL
+  `area_id` int(11) NOT NULL,
+  `data_nascimento` date NOT NULL,
+  `salario` decimal(8,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `professor`
 --
 
-INSERT INTO `professor` (`id`, `nome`, `area_id`) VALUES
-(1, 'Osshiro', 1),
-(2, 'Jiyan', 2),
-(3, 'Thales Duarte', 3),
-(4, 'Vladimir', 2),
-(5, 'Celeny', 4),
-(6, 'Jonathan', 5),
-(7, 'Jeferson', 6),
-(8, 'Frederico da Silva', 7),
-(9, 'Cassima', 1),
-(28, 'teste', 17),
-(31, 'novissimo', 3),
-(32, 'testes testes', 7),
-(33, 'fulano', 2);
+INSERT INTO `professor` (`id`, `nome`, `area_id`, `data_nascimento`, `salario`) VALUES
+(1, 'Márcio Osshiro', 1, '2010-01-15', '1000.00'),
+(2, 'Jiyan Yari', 7, '2010-10-10', '1000.00'),
+(3, 'Thales Duarte', 3, '0000-00-00', '0.00'),
+(5, 'Celeny', 4, '0000-00-00', '0.00'),
+(6, 'Jonathan', 17, '0000-00-00', '0.00'),
+(7, 'Jeferson', 6, '0000-00-00', '0.00'),
+(9, 'Cassima', 1, '0000-00-00', '0.00'),
+(37, 'Outro Professor', 3, '0000-00-00', '0.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Márcio Osshiro', 'marcio.osshiro@ifms.edu.br', NULL, '$2y$10$6V5eEqCc1P8vosqZKfBfp.yE4liDb2DmFUEajyTmjRb5893xu8LJm', NULL, '2019-09-25 06:28:01', '2019-09-25 06:28:01');
 
 --
 -- Índices para tabelas despejadas
@@ -117,10 +171,29 @@ ALTER TABLE `area`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
 -- Índices para tabela `professor`
 --
 ALTER TABLE `professor`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
 -- AUTO_INCREMENT de tabelas despejadas
@@ -136,13 +209,25 @@ ALTER TABLE `aluno`
 -- AUTO_INCREMENT de tabela `area`
 --
 ALTER TABLE `area`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de tabela `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+
+--
+-- AUTO_INCREMENT de tabela `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
